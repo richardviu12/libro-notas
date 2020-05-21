c1cdb558ec4f874de1a5ea6e480351a6ea704f7b
Ricardo Villagrán <richardalex917@gmail.com>
2020-05-21 01:46:47 -0600

    commit 2: Modulo de usuarios funcionando con bd, mvc completo

---
+++ resources/views/usuarios.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+
+@section('content')
+
+<table class="table">
+  <thead class="thead-dark">
+    <tr>
+      <th scope="col">ID</th>
+      <th scope="col">Nomre</th>
+      <th scope="col">Email</th>
+    </tr>
+  </thead>
+  <tbody>
+  @foreach($users as $user)
+        <tr>
+            <th scope="row">{{$user->id}}</th>
+            <td>{{$user->name}}</td>
+            <td>{{$user->email}}</td>
+        </tr>
+   @endforeach
+  </tbody>
+</table>
+
+
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,4 @@ use Illuminate\Support\Facades\Route;
 Auth::routes();
 
 Route::get('/', 'HomeController@index')->name('home');
+Route::get('/usuarios', 'UserController@index');
